ae9942ea5cbbd4ac06346a5085ef8f12c342a034
Adrian.Swoboda <adrian.swoboda93@gmail.com>
2022-05-20 20:33:21 +0200

    add product counter

--- shopping.py
+++ shopping.py
@@ -10,3 +10,5 @@ for shop in shopping:
     shopping_list = [product.capitalize() for product in shopping[shop]]
     print("Ide do", shop.capitalize(), "aby zakupic:", shopping_list)
     sum_of_product += len(shopping[shop])
+
+print("Do kupienia mam", sum_of_product, "produktów")
