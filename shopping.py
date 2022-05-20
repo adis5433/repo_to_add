37466a1ce1ed7a9c856f45e89aef3f99ea7ed050
Adrian.Swoboda <adrian.swoboda93@gmail.com>
2022-05-20 20:29:55 +0200

    add dict init

---
+++ shopping.py
@@ -0,0 +1,5 @@
+shopping = {
+    "warzywniak": ["kapusta", "cebula", "czosnek", "szczypiorek"],
+    "piekarnia": ["cheb", "bulki", "bagietka"],
+    "miesny": ["lopatka", "piers z kurczaka", "stek"],
+}
